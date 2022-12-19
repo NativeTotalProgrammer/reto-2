dcaf3091514a42d2b375bed229391e63a2ad3bd8
Luis Alberto Villar Ortiz <lu.villarortiz@gmail.com>
2022-12-19 17:00:17 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const array1 = ['O','Q','R','S'];
+
+function findMissingLetter(array) {
+  let startLetter = array[0].charCodeAt();
+  for (let i = 0; i < array.length; i++) {
+    console.log(startLetter + i, array[i].charCodeAt());
+    // if (startLetter + i !== array[i].charCodeAt()) {
+    //   return String.fromCharCode(startLetter + i);
+    // };
+  };
+};
+
+findMissingLetter(array1);
